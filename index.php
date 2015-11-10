f49e50ab68c6ee76250e3ea7f2251f57cc20b9bb
jiajia <jiajia@jiajia-PC>
2015-11-10 21:31:24 +0100

    empezando

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body style="background-color:  forestgreen">
+     
+                 
+                    
+        <div>
+            <form action="" style="margin-left: 50%; margin-top: 15%;width: 250px" >
+                                <div style=" height: 40px;width: 250px;background-color:  #ccccff;">
+                                    <p style="font-style:  oblique;padding-top: 10px">Sign in</p>
+                            </div>
+                                <div >
+                                    Username : <br>
+                                    <input   name="username" type="text" style="margin-top: 10px ; width:250px;height: 30px "/>
+                                </div>
+                                <div >
+                                    Password : <br>
+                                    <input  name="password" type="password" style="margin-top: 10px ;width:250px;height: 30px "/>
+                                </div>
+                                <br>
+                                <button class="btn btn-primary" name="submit" type="submit" value=" Login "  style="width:80px;height: 45px ">Log in</button>
+                              
+                              <a href="registro.php ">  <button type="button" class="btn btn-default" style="width:80px;height: 45px;margin-left: 80px " >Registrarse</button>  </a>
+                            </form>
+                        </div>
+                    </div>
+</div>
+    </body>
+</html>
---
+++ registro.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+          <div >
+     <form  style="margin-left:33%; margin-top: 15%;width: 250px" >
+                                <div style=" height: 40px;width: 250px;">
+                                    <b>Create your personal account </b>
+                            </div>
+                                <div >
+                                    Username : <br>
+                                    <input   name="username" type="text" style="margin-top: 10px ; width:250px;height: 30px "/>
+                                </div>
+                                <div >
+                                    Email address : <br>
+                                    <input  name="email" type="text" style="margin-top: 10px ; width:250px;height: 30px "/>
+                                </div>
+                                           <div >
+                                    Password : <br>
+                                    <input  name="password" type="password" style="margin-top: 10px ; width:250px;height: 30px "/>
+                                </div>
+                                <br>
+                                <button class="btn btn-primary" name="submit" type="submit" value=" registra " >Registra</button>
+                              
+                            </form>
+                        </div>
+                    </div>
+</div>
+    </body>
+</html>

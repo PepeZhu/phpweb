e7fdbf45d2237e70cefca973e32641112c63a498
Administrator <Administrator@andro20>
2015-11-11 13:34:48 +0100

    empezando configuracion

--- index.php
+++ index.php
@@ -7,29 +7,40 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="css/bootstrap-theme.css" />
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css"/>
+        <link rel="stylesheet" href="js/bootstrap.min.js"/>
+        <link rel="stylesheet" href="css/bootstrap.css" />
+        <link rel="stylesheet" href="css/bootstrap-theme.css.map" />
+        <link rel="stylesheet" href="js/bootstrap.js" />
+        <link rel="stylesheet" href="js/bootstrap.min.js" />
+      <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+
+  
+
+<title></title>
+        
     </head>
-    <body style="background-color:  forestgreen">
+    <body style="background-color: #c9e2b3">
      
                  
                     
         <div>
-            <form action="" style="margin-left: 50%; margin-top: 15%;width: 250px" >
-                                <div style=" height: 40px;width: 250px;background-color:  #ccccff;">
-                                    <p style="font-style:  oblique;padding-top: 10px">Sign in</p>
-                            </div>
+            <form action="venta.php" style="margin-left: 40%; margin-top: 15%;width: 250px" >
+               
                                 <div >
-                                    Username : <br>
-                                    <input   name="username" type="text" style="margin-top: 10px ; width:250px;height: 30px "/>
+                                    <b>Username :</b> <br>
+                                    <input  name="username" type="text" style="margin-top: 10px ; width:250px;height: 30px "/>
                                 </div>
                                 <div >
-                                    Password : <br>
+                                    <b >Password :</b><br>
                                     <input  name="password" type="password" style="margin-top: 10px ;width:250px;height: 30px "/>
                                 </div>
                                 <br>
-                                <button class="btn btn-primary" name="submit" type="submit" value=" Login "  style="width:80px;height: 45px ">Log in</button>
+                                <button class="btn btn-primary" name="loin" type="submit" value=" Login "  style="width:80px;height: 45px ">Log in</button>
                               
-                              <a href="registro.php ">  <button type="button" class="btn btn-default" style="width:80px;height: 45px;margin-left: 80px " >Registrarse</button>  </a>
+                              <a href="registro.php ">  <button type="button" class="btn btn-danger " style="width:100px;height: 45px;margin-left: 50px " >Registrarse</button>  </a>
                             </form>
                         </div>
                     </div>
--- registro.php
+++ registro.php
@@ -7,17 +7,29 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
+       
         <title></title>
     </head>
     <body>
-          <div >
+        
+ 
+  
+        
+          <div >        
      <form  style="margin-left:33%; margin-top: 15%;width: 250px" >
                                 <div style=" height: 40px;width: 250px;">
                                     <b>Create your personal account </b>
                             </div>
                                 <div >
                                     Username : <br>
-                                    <input   name="username" type="text" style="margin-top: 10px ; width:250px;height: 30px "/>
+                                    <input   name="user" type="text" style="margin-top: 10px ; width:250px;height: 30px "/>
                                 </div>
                                 <div >
                                     Email address : <br>
@@ -25,10 +37,10 @@ and open the template in the editor.
                                 </div>
                                            <div >
                                     Password : <br>
-                                    <input  name="password" type="password" style="margin-top: 10px ; width:250px;height: 30px "/>
+                                    <input  name="passw" type="text" style="margin-top: 10px ; width:250px;height: 30px "/>
                                 </div>
                                 <br>
-                                <button class="btn btn-primary" name="submit" type="submit" value=" registra " >Registra</button>
+                                <button type="button"  name="registrar" class="btn btn-danger " style="width:100px;height: 45px;margin-left: 50px " >Registra</button>
                               
                             </form>
                         </div>
